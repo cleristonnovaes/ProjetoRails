aeb50c29825e263b5177dcba4945b93400e32b51
Cleriston <novaes.cleriston@gmail.com>
2021-09-08 16:57:30 -0300

    layouts

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -3,4 +3,9 @@ class ProdutosController < ApplicationController
         @produtosnome = Produto.order(:nome).limit 5
         @produtospreco = Produto.order(:preco).limit 2
     end
+
+    def create
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+        produto = Produto.create valores
+    end
 end
