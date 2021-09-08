89e7e01a4839e83cd8e55dba92cfcce593b608b7
Cleriston <novaes.cleriston@gmail.com>
2021-09-08 14:13:29 -0300

    commit 08-09-21

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,2 +1,6 @@
 class ProdutosController < ApplicationController
+    def index
+        @produtosnome = Produto.order(:nome).limit 5
+        @produtospreco = Produto.order(:preco).limit 2
+    end
 end
