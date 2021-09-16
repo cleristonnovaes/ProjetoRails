ac29174e0e720cd393e56af5076ac448aba378d2
Cleriston <novaes.cleriston@gmail.com>
2021-09-16 11:59:06 -0300

    updates formularios

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,8 +4,20 @@ class ProdutosController < ApplicationController
         @produtospreco = Produto.order(:preco).limit 2
     end
 
+    def busca
+        @nome_a_buscar = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+    end
+
     def create
         valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
         produto = Produto.create valores
+        redirect_to root_url
+    end
+
+    def destroy
+        id = params[:id]
+        Produto.destroy id
+        redirect_to root_url
     end
 end
