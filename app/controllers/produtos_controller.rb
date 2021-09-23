a37d4195d286081788137c3b3b9462be1762cd30
Cleriston <novaes.cleriston@gmail.com>
2021-09-23 11:02:05 -0300

    update pages

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -6,7 +6,7 @@ class ProdutosController < ApplicationController
 
     def new
         @produto = Produto.new
-        @departamentos = Departamento.all 
+        renderiza_new
     end
 
     def busca
@@ -15,11 +15,23 @@ class ProdutosController < ApplicationController
     end
 
     def edit
-        id = params[:id]
-        @produto = Produto.find(id)
-        @departamentos = Departamento.all 
-        render :new
+        set_produto
+        renderiza_new
+        
     end
+   
+
+    def update
+        set_produto
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
+        if @produto.update valores
+            flash[:notice] = "Produto atualizado com sucesso"
+            redirect_to root_url
+        else
+            renderiza_new
+        end
+    end
+
 
     def create
         valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
@@ -28,13 +40,26 @@ class ProdutosController < ApplicationController
             flash[:notice] = "Produto salvo com sucesso"
             redirect_to root_url
         else
-            render :new
+            renderiza_new
         end
     end
 
     def destroy
-        id = params[:id]
-        Produto.destroy id
+        set_produto
+        @produto.destroy
         redirect_to root_url
     end
+
+    private
+
+    def renderiza_new
+        @departamentos = Departamento.all 
+        render :new
+    end
+
+    def set_produto
+        id = params[:id]
+        @produto = Produto.find(id)
+    end
+
 end
